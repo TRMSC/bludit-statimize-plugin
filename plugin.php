e351b5d300a93d8b6dbd65622da8168e992a6382
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-11-30 22:45:35 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -12,18 +12,23 @@ class Statimize extends Plugin {
   
   
         public function form() {
-          
+                
+                $html = '<textarea class="form-control" rows="3" name="concealItems" id="concealItems">'.$this->getValue('concealItems').'</textarea>';
+        
+                return $htmml;
         }
   
   
         public function concealItems() {
   
-                $conceal = 
+                $html = 
                   'let bin = document.getElementsByClassName('nav-link');' +
                   'for (let i=0; i<bin.length; i++) {' +
-                      'if (bin[i].text == 'XXX') {' + // variable is needed
+                      'if (bin[i].text == $this.getValue('concealItems')) {' +
                           'bin[i].parentElement.style.display = 'none';' +
                       '}' +
                   '}';
+          
+                return $html;
 
        }
