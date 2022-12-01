1569672b32a6424e6887fc41f63598b54c540c76
TRMSC <solidware@web.de>
2022-12-01 22:15:32 +0100

    remove comment

--- statimize.js
+++ statimize.js
@@ -6,12 +6,3 @@ removeEntries = (item) => {
         }
     }
 }
-
-/*
-    let bin = document.getElementsByClassName("nav-link");
-    for (let i=0; i<bin.length; i++) {
-        if (bin[i].text == $this.getValue("concealItems")) {
-            bin[i].parentElement.style.display = "none";
-        }
-    }
-*/
