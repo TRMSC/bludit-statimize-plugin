fa56577b29fb30f2017548960ec8307e63c73156
TRMSC <solidware@web.de>
2022-12-02 19:32:49 +0100

    optimize url building

--- plugin.php
+++ plugin.php
@@ -17,7 +17,10 @@ class pluginStatimize extends Plugin {
                 foreach ($concealItems as $value) {
                     $entry .= '.nav-link[href*="'.$value.'"]{display: none;} ';
                 }
-                $file = PATH_PLUGINS.'statimize/remove.css';
+
+                $folder = explode('/', __FILE__);
+                $folder = $folder[count($folder)-2];
+                $file = PATH_PLUGINS.$folder.'/remove.css';
                 file_put_contents($file, $entry);
 
                 global $L;
@@ -30,10 +33,10 @@ class pluginStatimize extends Plugin {
 
         public function siteHead() {
 
-                $file = $this->workspace().'remove.css';
-                $file = explode('/', $file);
-                $file = '../'.$file[count($file) - 4].'/'.$file[count($file)-3].'/'.$file[count($file) - 2].'/'.$file[count($file) - 1];
-                $file = HTML_PATH_PLUGINS.'statimize/remove.css';
+                $folder = explode('/', __FILE__);
+                $folder = $folder[count($folder)-2];
+                $file = HTML_PATH_PLUGINS.$folder.'/remove.css';
+
                 $html = '<link rel="stylesheet" type="text/css" href="..'.$file.'"/>';
 
                 return $html;
