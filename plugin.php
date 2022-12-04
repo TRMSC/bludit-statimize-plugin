6a5e9cc4f895b89572b3736cf59eee29165ec7a1
TRMSC <solidware@web.de>
2022-12-05 00:01:17 +0100

    construct for handeling footer

--- plugin.php
+++ plugin.php
@@ -5,13 +5,16 @@ class pluginStatimize extends Plugin {
         public function init() {
           
                 $this->dbFields = array(
-                        'concealItems' => ''
+                        'concealItems' => '',
+                        'supplementItems' => ''
                 );
           
         }
   
   
         public function form() {
+
+                // NAVBAR FILE
 
                 $concealItems = preg_split("/\r\n|\n|\r/", $this->getValue("concealItems"));
                 foreach ($concealItems as $value) {
@@ -23,6 +26,8 @@ class pluginStatimize extends Plugin {
                 $file = PATH_PLUGINS.$folder.'/remove.css';
                 file_put_contents($file, $entry);
 
+                // NAVBAR STORE
+
                 global $L;
                 $html = '<h4>'.$L->get('remove-header').'</h4>';
                 $html .= '<p>'.$L->get('remove-l1').'<br>';
@@ -31,12 +36,40 @@ class pluginStatimize extends Plugin {
                         .$this->getValue('concealItems').'</textarea>';
                 $html .= '<sub>'.$L->get('remove-tip').'</sub>';
 
+                // FOOTER FILE
+
+                $item = 'const re = /\[([^\[]*)\]\((.*)\)/; ';
+                $item .= 'window.onload = funtion () { ';
+                $item .= 'items = [ ';
+
+                $supplementItems = preg_split("/\r\n|\n|\r/", $this->getValue("supplementItems"));
+                foreach ($supplementItems as $value) {
+                        $item .= '"'.trim($value).'", ';
+                        //.pregmatch($re);
+                }
+                $item .= ']; ';
+                $item .= '}';
+
+                $fileJs = PATH_PLUGINS.$folder.'/add.js';
+                file_put_contents($fileJs, $item);
+
+                // FOOTER STORE
+
+                $html .= '<hr><h4>'.$L->get('add-header').'</h4>';
+                $html .= '<p>'.$L->get('add-l1').'<br>';
+                $html .= $L->get('add-l2').'</p>';
+                $html .= '<textarea class="form-control" rows="3" name="supplementItems" id="supplementItems">'
+                        .$this->getValue('supplementItems').'</textarea>';
+                $html .= '<sub>'.$L->get('add-tip').'</sub>';
+
                 return $html;
 
         }
 
 
         public function siteHead() {
+
+                // NAVBAR
 
                 $folder = explode('/', __FILE__);
                 $folder = $folder[count($folder)-2];
@@ -47,5 +80,6 @@ class pluginStatimize extends Plugin {
                 return $html;
 
         }
+
   
 }
