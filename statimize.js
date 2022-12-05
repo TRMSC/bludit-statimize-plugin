58897209ecb13126ed78859b11fbacdd69304cd0
TRMSC <solidware@web.de>
2022-12-05 07:28:51 +0100

    create test for first item

--- statimize.js
+++ statimize.js
@@ -1,5 +1,11 @@
 const re = /\[([^\[]*)\]\((.*)\)/;
 
 window.onload = () => {
-    console.log(items);
+    // for...
+    let myMatch = items[0].match(re);
+    console.log(myMatch);
+    if (myMatch.length === 3) {
+        console.log('text: ' + myMatch[1]);
+        console.log('url: ' + myMatch[2]);
+    }
 };
