3f0fe38c7acd996e9f67f071fdb3e4fbd038c96d
TRMSC <solidware@web.de>
2022-12-05 07:40:47 +0100

    add condition

--- statimize.js
+++ statimize.js
@@ -3,8 +3,7 @@ const re = /\[([^\[]*)\]\((.*)\)/;
 window.onload = () => {
     for (let i=0; i<items.length; i++) {
         myMatch = items[i].match(re);
-        console.log(myMatch);
-        if (myMatch.length == 3) {
+        if (myMatch != null && myMatch.length == 3) {
             console.log('text: ' + myMatch[1]);
             console.log('url: ' + myMatch[2]);
         }
