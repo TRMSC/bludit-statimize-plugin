0b49309d83a13274676539b706c98a408e9ddc13
TRMSC <solidware@web.de>
2022-12-05 14:37:04 +0100

    approach final location

--- statimize.js
+++ statimize.js
@@ -5,13 +5,16 @@ window.onload = () => {
     for (let i=0; i<items.length; i++) {
         myMatch = items[i].match(re);
         if (myMatch != null && myMatch.length == 3) {
-            html += '<a href="' + myMatch[2] + '">' + myMatch[1] + '</a>';
+            html += '<a class="ml-5 text-warning" href="' + myMatch[2] + '">' + myMatch[1] + '</a>';
             console.log('text: ' + myMatch[1]);
             console.log('url: ' + myMatch[2]);
         }
     }
-    console.log(html);
-    let linkContainer = document.createElement('p');
-    linkContainer.innerHTML = html;
-    document.getElementById('footer-src').append(linkContainer);
+    if (html.length > 0) {
+        let linkContainer = document.createElement('span');
+        linkContainer.innerHTML = html;
+        //document.getElementById('footer-src').append(linkContainer);
+        let place = document.getElementsByTagName('footer')[0].getElementsByTagName('p')[0];
+        place.append(linkContainer);
+    }
 };
