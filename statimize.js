4c357c640c456d294154229eb5f0fdc6c9bd210e
TRMSC <solidware@web.de>
2022-12-06 13:16:53 +0100

    remove unused parts

--- statimize.js
+++ statimize.js
@@ -6,8 +6,6 @@ window.onload = () => {
         myMatch = items[i].match(re);
         if (myMatch != null && myMatch.length == 3) {
             html += '<a class="ml-5 text-warning" href="' + myMatch[2] + '">' + myMatch[1] + '</a>';
-            console.log('text: ' + myMatch[1]);
-            console.log('url: ' + myMatch[2]);
         }
     }
     if (html.length > 0) {
