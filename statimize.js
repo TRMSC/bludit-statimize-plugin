ea84147761cf44ed60cfac85ac5a99538299ab16
TRMSC <solidware@web.de>
2022-12-10 13:17:13 +0100

    add condition and change classes

--- statimize.js
+++ statimize.js
@@ -5,7 +5,7 @@ prepareFooter = (theme) => {
     for (let i=0; i<items.length; i++) {
         myMatch = items[i].match(re);
         if (myMatch != null && myMatch.length == 3) {
-            html += '<a class="ml-5 text-warning" href="' + myMatch[2] + '">' + myMatch[1] + '</a>';
+            html += '<a class="text-warning" href="' + myMatch[2] + '">' + myMatch[1] + '</a>';
         }
     }
     //if (html.length === 0) return; // UNCOMMENT LATER
@@ -16,11 +16,25 @@ prepareFooter = (theme) => {
 
 editSmart = (html) => {
     console.log('smart theme is active');
+
+    // Add footer links
+    if (html.length !== 0) {
+        let footerLinks = document.getElementById('footer-links');
+        footerLinks.classList.add('col', 'footer-col', 'footer-right');
+        footerLinks.innerHTML = html;
+        let footerSource = document.getElementById('footer-source');
+        footerSource.classList.remove('footer-right');
+        footerSource.classList.add('footer-center');
+    }
+
+    // Add footer text
+    /*
     let footer = document.getElementById('footer-src');
     let textArea = document.createElement('div');
     textArea.classList.add('row', 'text-white');
     textArea.textContent = text;
     footer.after(textArea);
+    */
 };
 
 editBludit = (html) => {
